f0c8c40db95fe784eca8b62ac6e8c11f556bb133
gonzalo727 <gbquiroga@escuelasproa.edu.ar>
2024-08-21 11:21:13 -0300

    Add files via upload

--- autos.py
+++ autos.py
@@ -11,20 +11,31 @@ class Fabrica:
     def mostrar_info(self):
         print(f"Llantas: {self.llantas}, Color: {self.color}, Precio: {self.precio}")
 
+    def aplicar_descuento(self):
+        if self.precio > 100000:
+            descuento = self.precio * 0.10
+            precio_con_descuento = self.precio - descuento
+            return precio_con_descuento
+        elif self.precio<100000:
+            print("El producto no alcanza el valor de 100.000. Por ende, no se aplica el descuento. ")
+
 class Moto(Fabrica):
     def __init__(self, color, precio):
         super().__init__(2, color, precio)
 
-class auto(Fabrica):
+class Auto(Fabrica):
     def __init__(self, color, precio):
         super().__init__(4, color, precio)
 
-
+# Crear objetos
 moto = Moto("Rojo", 1500)
-auto = auto("Azul", 25000)
+auto = Auto("Azul", 250000)  # Cambié el precio a 250,000 para mostrar el descuento
 
+# Mostrar información y precios con descuento si aplican
 print("Información de la moto:")
 moto.mostrar_info()
+print(f"Precio con descuento: {moto.aplicar_descuento()}")
 
-print("Información del carro:")
+print("\nInformación del carro:")
 auto.mostrar_info()
+print(f"Precio con descuento: {auto.aplicar_descuento()}")
